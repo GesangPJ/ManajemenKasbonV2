6c20c79761e647830e2e19d44a52d7bb4c1211ef
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 21:42:24 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(IsAdmin::class);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
