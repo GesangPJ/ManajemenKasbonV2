8d960b7beab842964e02f6cde3588783209de3cf
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 17:38:34 +0700

    Create KasbonUpdateRequest.php

---
+++ app/Http/Requests/KasbonUpdateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class KasbonUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status_r' => ['required', 'string', 'max:255'],  // Setuju / Tolak
+            'status_b' => ['required', 'string', 'max:255'],  // Lunas / Belum Lunas
+        ];
+    }
+}
