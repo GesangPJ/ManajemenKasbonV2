26c5757dbc1bd4616f403cabcd6bc0b47e607547
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 19:44:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
@@ -26,3 +26,13 @@ Route::get('request',function(){
 Route::get('bayar', function(){
     return view('bayarkasbon',['halaman'=>'Bayar Kasbon']);
 })->middleware(['auth', 'verified'])->name('bayar');
+
+Route::get('/admin-dashboard', function () {
+    // Return the view for the admin dashboard
+    return view('dashboard');
+})->middleware(['auth', 'is_admin'])->name('admin-dashboard');
+
+Route::get('/karyawan-dashboard', function () {
+    // Return the view for the user dashboard
+    return view('karyawan-dashboard');
+})->middleware(['auth'])->name('karyawan-dashboard');
