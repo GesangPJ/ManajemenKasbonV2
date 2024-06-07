4d369e630db46b11c3ae1eb7db5a15c817900e11
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 21:42:38 +0700

    Create dashboard.blade.php

---
+++ resources/views/karyawan/dashboard.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard Karyawan') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+
+                    <p>User ID : {{ Auth::user()->id }}
+                    </p>
+                    <p>Email : {{ Auth::user()->email }} </p>
+                    <p>User Type : {{ Auth::user()->is_admin }}</p>
+                </div>
+            </div><br>
+            <x-karyawantable></x-karyawantable>
+        </div>
+    </div>
+</x-app-layout>
+
