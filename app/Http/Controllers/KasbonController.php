b8b48d85042b28630768af909feb3134140df63a
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 17:53:08 +0700

    Update KasbonController.php

--- app/Http/Controllers/KasbonController.php
+++ app/Http/Controllers/KasbonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kasbon;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
@@ -17,16 +18,42 @@ class KasbonController extends Controller
         ]);
     }
 
-    public function update(KasbonUpdateRequest $request): RedirectResponse
+    public function TambahKasbon(Request $request): RedirectResponse
+    {
+        $kasbon = new Kasbon;
+        $kasbon->user_id = $request->session()->get('id');
+        $kasbon->jumlah = $request->jumlah;
+        $kasbon->metode = $request->metode;
+        $kasbon->keterangan = $request->keterangan;
+
+        return redirect('/inputkasbon');
+    }
+
+    public function update_status_r(KasbonUpdateRequest $request): RedirectResponse
     {
         $kasbon = $request->kasbon();
         $kasbon->fill($request->validated());
 
-        // Update status_r and status_b
+        // Update status_r
         $kasbon->status_r = $request->status_r;
-        $kasbon->status_b = $request->status_b;
+
 
         // Insert session ID into admin_id field
+        $kasbon->admin_id = $request->session()->get('id');
+
+        $kasbon->save();
+
+        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+    }
+
+    public function update_status_b(KasbonUpdateRequest $request): RedirectResponse
+    {
+        $kasbon = $request->kasbon();
+        $kasbon->fill($request->validated());
+
+        // Update status_b
+        $kasbon->status_b = $request->status_b;
+
         $kasbon->admin_id = $request->session()->get('id');
 
         $kasbon->save();
