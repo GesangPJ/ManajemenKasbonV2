aa80f515578034a6c5fd0313b8eba366977a865b
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 16:59:04 +0700

    Create admintable.blade.php

---
+++ resources/views/components/admintable.blade.php
@@ -0,0 +1,61 @@
+<!--Card-->
+<div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+
+    <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+        <thead>
+            <tr>
+                <th data-priority="1">Nama Karyawan</th>
+                <th data-priority="2">Jumlah</th>
+                <th data-priority="3">Metode</th>
+                <th data-priority="4">S.Request</th>
+                <th data-priority="5">S.Bayar</th>
+                <th data-priority="6">Keterangan</th>
+                <th data-priority="7">Admin</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+                <td>Gesang Paudra Jaya</td>
+            </tr>
+
+            <!-- Rest of your data (refer to https://datatables.net/examples/server_side/ for server side processing)-->
+
+            <tr>
+                <td>Donna Snider</td>
+                <td>Customer Support</td>
+                <td>New York</td>
+                <td>27</td>
+                <td>2011/01/25</td>
+                <td>$112,000</td>
+                <td>Gesang Paudra Jaya</td>
+            </tr>
+        </tbody>
+
+    </table>
+
+
+</div>
+<!--/Card-->
+
+<!-- jQuery -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+<!--Datatables -->
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+<script>
+    $(document).ready(function() {
+
+        var table = $('#example').DataTable({
+                responsive: true
+            })
+            .columns.adjust()
+            .responsive.recalc();
+    });
+</script>
