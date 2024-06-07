f24b7580ed67e2c0472490a0a759bafe45a70c35
Gesang Paudra J <g.paudra@gmail.com>
2024-06-07 16:59:50 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Admin') }}
         </h2>
     </x-slot>
 
@@ -10,8 +10,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+
+                    <p>User ID : {{ Auth::user()->id }}
+                    </p>
+                    <p>Email : {{ Auth::user()->email }} </p>
+                    <p>User Type : {{ Auth::user()->is_admin }}</p>
                 </div>
-            </div>
+            </div><br>
+            <x-admintable></x-admintable>
         </div>
     </div>
 </x-app-layout>
