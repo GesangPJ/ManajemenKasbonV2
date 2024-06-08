feeacd696291e5f101d632f320867312b6ba7d09
Gesang Paudra J <g.paudra@gmail.com>
2024-06-08 14:52:45 +0700

    working request

---
+++ app/Http/Requests/KasbonRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class KasbonRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id',
+            'jumlah' => ['required'],  // Jumlah Kasbon
+            'metode' => ['required'],  // Metode Pembayaran
+            'keterangan' => ['required'], // Keterangan Kasbon
+        ];
+    }
+}
