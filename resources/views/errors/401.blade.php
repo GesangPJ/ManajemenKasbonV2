8e180516039d5ba3e99a55c5ffce104d6ab506b7
Gesang Paudra J <g.paudra@gmail.com>
2024-06-10 20:46:19 +0700

    Update 401.blade.php

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -15,7 +15,7 @@
         <p class="mt-6 text-base leading-7 text-white">You need authorization to enter this page.</p>
         <p class="mt-6 text-base leading-7 text-white">Anda memerlukan izin untuk mengakses halaman ini.</p>
         <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="/dashboard" class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">&laquo; Go To Dashboard</a>
+            <a href="/" class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">&laquo; Back To Login</a>
         </div>
     </main>
 </body>
