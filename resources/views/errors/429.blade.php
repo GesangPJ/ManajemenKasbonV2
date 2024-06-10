f9bce109b49e0c6a022f9ecf715af8ef8cb7dd28
Gesang Paudra J <g.paudra@gmail.com>
2024-06-10 20:20:09 +0700

    generate error pages

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,22 @@
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GPAUDRA - Error 429</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+<body class="h-full flex items-center justify-center bg-black">
+    <main class="text-center">
+        <h1 class="text-9xl font-extrabold text-red-500">429</h1>
+        <h2 class="mt-4 text-xl font-bold tracking-tight text-yellow-300 sm:text-5xl">Too Many Requests</h2>
+        <p class="mt-6 text-base leading-7 text-white">Sorry, we can't show the page, try again later.</p>
+        <p class="mt-6 text-base leading-7 text-white">Maaf, kami tidak bisa menampilkan halaman, coba beberapa saat lagi.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="/dashboard" class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">&laquo; Go To Dashboard</a>
+        </div>
+    </main>
+</body>
+</html>
