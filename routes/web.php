3d3c8ec492b8656b3eee2387f0f6647a96a53806
Gesang Paudra J <g.paudra@gmail.com>
2024-06-10 20:46:33 +0700

    protect admin page

--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,11 @@ Route::get('/detail/{kasbon:id}', function(Kasbonview $kasbon){
 
 Route::get('/admin-request', function(){
     return view('admin.viewrequest', ['requestkasbon'=>Kasbonview::viewRequest()]);
-})->middleware(['auth', 'verified'])->name('admin-request');
+})->middleware(['auth', 'verified','admin'])->name('admin-request');
 
 Route::get('/admin-bayar', function(){
     return view('admin.viewbayar', ['bayarkasbon'=>Kasbonview::viewBayar()]);
-})->middleware(['auth', 'verified'])->name('admin-bayar');
-
+})->middleware(['auth', 'verified', 'admin'])->name('admin-bayar');
 
 
 require __DIR__.'/auth.php';
