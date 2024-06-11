d19e74bf9f1c1f7d4d8b00cc4e2fa9989ebf30b6
Gesang Paudra J <g.paudra@gmail.com>
2024-06-11 19:02:23 +0700

    fix typo

--- resources/views/components/datatablescript.blade.php
+++ resources/views/components/datatablescript.blade.php
@@ -15,7 +15,7 @@
 
 <!-- datatables option and addons-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></scrip>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.colVis.min.js"></script>
