5dae21500e1da8b5ea92f9828c0396a45d960cb0
Gesang Paudra J <g.paudra@gmail.com>
2024-06-11 12:55:24 +0700

    working save to pdf and excel with filter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -217,13 +217,46 @@
 <!-- jQuery untuk tabel-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
-<!--Datatables -->
+<!--Datatables
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.colVis.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/datetime/1.5.2/js/dataTables.dateTime.min.js"></script>
+<script src="https://cdn.datatables.net/fixedheader/4.0.1/js/dataTables.fixedHeader.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/scroller/2.4.3/js/dataTables.scroller.min.js"></script>
+<script src="https://cdn.datatables.net/searchpanes/2.3.1/js/dataTables.searchPanes.min.js"></script>
+
 <script>
     $(document).ready(function() {
-
         var table = $('#tablekasbon').DataTable({
+            layout: {
+                    bottomStart: {
+                        buttons: [
+                            {
+                                extend: 'pdf',
+                                text: 'Simpan Ke PDF',
+                            },
+                            {
+                                extend:'spacer',
+
+                            },
+                            {
+                                extend: 'excelHtml5',
+                                text: 'Simpan Ke Excel',
+                                autoFilter: true,
+                            },
+                        ],
+
+                    }
+                },
                 responsive: true
             })
             .columns.adjust()
